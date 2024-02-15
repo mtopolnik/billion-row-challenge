52e5f2dc4545d174a608984579af9b9e6ef694b2
Marko Topolnik <marko.topolnik@gmail.com>
2024-02-15 16:40:42 +0100

    Cleanup

--- src/SimplifyData.java
+++ src/SimplifyData.java
@@ -7,30 +7,11 @@ import java.io.FileWriter;
 import java.util.Map;
 
 public class SimplifyData {
-    public static void x(String[] args) throws Exception {
-        try (var rows = new CSVReaderHeaderAware(new FileReader("data/weather_stations.csv"))) {
-            while (true) {
-                Map<String, String> row = rows.readMap();
-                if (row == null) {
-                    break;
-                }
-                var city = row.get("city");
-                var lat = row.get("lat");
-                if (city.indexOf(';') != -1) {
-                    System.out.println("problem: " + city);
-                    System.exit(0);
-                }
-                System.out.format("%s: %s%n", city, lat);
-            }
-        }
-    }
-
     // Source: https://simplemaps.com/data/world-cities
     public static void main(String[] args) throws Exception {
         try (var rows = new CSVReaderHeaderAware(new FileReader("data/worldcities.csv"));
              var outFile = new BufferedWriter(new FileWriter("data/weather_stations.csv"))
         ) {
-//            outFile.write("\"city\",\"lat\"\n");
             while (true) {
                 Map<String, String> row = rows.readMap();
                 if (row == null) {
