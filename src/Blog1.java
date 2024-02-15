880f2ddf43e09480d58c54f2f375aab857dba976
Marko Topolnik <marko.topolnik@gmail.com>
2024-02-15 10:46:29 +0100

    Blog2

--- src/Blog1.java
+++ src/Blog1.java
@@ -1,8 +1,5 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
-import java.util.Comparator;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
@@ -14,23 +11,27 @@ import static java.util.stream.Collectors.summarizingDouble;
 public class Blog1 {
     public static void main(String[] args) throws Exception {
         var start = System.currentTimeMillis();
-var allStats = new BufferedReader(new FileReader("measurements.txt"))
-        .lines()
-        .parallel()
-        .collect(
-                groupingBy(line -> line.substring(0, line.indexOf(';')),
-                summarizingDouble(line ->
-                        parseDouble(line.substring(line.indexOf(';') + 1)))));
-var result = allStats.entrySet().stream().collect(Collectors.toMap(
-        Entry::getKey,
-        e -> {
-            var stats = e.getValue();
-            return String.format("%.1f/%.1f/%.1f",
-                    stats.getMin(), stats.getAverage(), stats.getMax());
-        },
-        (l, r) -> r,
-        TreeMap::new));
-System.out.println(result);
+        calculate();
         System.err.format("Took %,d ms\n", System.currentTimeMillis() - start);
+    }
+
+    private static void calculate() throws Exception {
+        var allStats = new BufferedReader(new FileReader("measurements.txt"))
+                .lines()
+                .parallel()
+                .collect(
+                        groupingBy(line -> line.substring(0, line.indexOf(';')),
+                                summarizingDouble(line ->
+                                        parseDouble(line.substring(line.indexOf(';') + 1)))));
+        var result = allStats.entrySet().stream().collect(Collectors.toMap(
+                Entry::getKey,
+                e -> {
+                    var stats = e.getValue();
+                    return String.format("%.1f/%.1f/%.1f",
+                            stats.getMin(), stats.getAverage(), stats.getMax());
+                },
+                (l, r) -> r,
+                TreeMap::new));
+        System.out.println(result);
     }
 }
---
+++ src/Blog2.java
@@ -0,0 +1,134 @@
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.nio.channels.FileChannel.MapMode;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
+
+public class Blog2 {
+    public static void main(String[] args) throws Exception {
+        var clockStart = System.currentTimeMillis();
+        final File file = new File("measurements.txt");
+        final long length = file.length();
+        final int chunkCount = Runtime.getRuntime().availableProcessors();
+        final var results = new StationStats[chunkCount][];
+        final var chunkStartOffsets = new long[chunkCount];
+        try (var raf = new RandomAccessFile(file, "r")) {
+            for (int i = 1; i < chunkStartOffsets.length; i++) {
+                var start = length * i / chunkStartOffsets.length;
+                raf.seek(start);
+                while (raf.read() != (byte) '\n') {
+                }
+                start = raf.getFilePointer();
+                chunkStartOffsets[i] = start;
+            }
+            final var mappedFile = raf.getChannel().map(MapMode.READ_ONLY, 0, length, Arena.global());
+            var threads = new Thread[chunkCount];
+            for (int i = 0; i < chunkCount; i++) {
+                final long chunkStart = chunkStartOffsets[i];
+                final long chunkLimit = (i + 1 < chunkCount) ? chunkStartOffsets[i + 1] : length;
+                threads[i] = new Thread(new ChunkProcessor(
+                        mappedFile.asSlice(chunkStart, chunkLimit - chunkStart), results, i));
+            }
+            for (var thread : threads) {
+                thread.start();
+            }
+            for (var thread : threads) {
+                thread.join();
+            }
+        }
+        var totalsMap = new TreeMap<String, StationStats>();
+        for (var statsArray : results) {
+            for (var stats : statsArray) {
+                totalsMap.merge(stats.name, stats, (old, curr) -> {
+                    old.count += curr.count;
+                    old.sum += curr.sum;
+                    old.min = Math.min(old.min, curr.min);
+                    old.max = Math.max(old.max, curr.max);
+                    return old;
+                });
+            }
+        }
+        System.out.println(totalsMap);
+        System.err.format("Took %,d ms\n", System.currentTimeMillis() - clockStart);
+    }
+
+    private static class ChunkProcessor implements Runnable {
+        private final MemorySegment chunk;
+        private final StationStats[][] results;
+        private final int myIndex;
+        private final Map<String, StationStats> statsMap = new HashMap<>();
+
+        ChunkProcessor(MemorySegment chunk, StationStats[][] results, int myIndex) {
+            this.chunk = chunk;
+            this.results = results;
+            this.myIndex = myIndex;
+        }
+
+        @Override public void run() {
+            for (var cursor = 0L; cursor < chunk.byteSize();) {
+                var semicolonPos = findByte(cursor, ';');
+                var newlinePos = findByte(semicolonPos + 1, '\n');
+                var name = stringAt(cursor, semicolonPos);
+                var temp = Double.parseDouble(stringAt(semicolonPos + 1, newlinePos));
+                var stats = statsMap.computeIfAbsent(name, k -> new StationStats(name));
+                var intTemp = (int) Math.round(10 * temp);
+                stats.sum += intTemp;
+                stats.count++;
+                stats.min = Math.min(stats.min, intTemp);
+                stats.max = Math.max(stats.max, intTemp);
+                cursor = newlinePos + 1;
+            }
+            results[myIndex] = statsMap.values().toArray(StationStats[]::new);
+        }
+
+        private long findByte(long cursor, int b) {
+            for (var i = cursor; i < chunk.byteSize(); i++) {
+                if (chunk.get(JAVA_BYTE, i) == b) {
+                    return i;
+                }
+            }
+            throw new RuntimeException(((char) b) + " not found");
+        }
+
+        private String stringAt(long start, long limit) {
+            return new String(
+                    chunk.asSlice(start, limit - start).toArray(JAVA_BYTE),
+                    StandardCharsets.UTF_8
+            );
+        }
+    }
+
+    static class StationStats implements Comparable<StationStats> {
+        String name;
+        long sum;
+        int count;
+        int min;
+        int max;
+
+        StationStats(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s=%.1f/%.1f/%.1f", name, min / 10.0, Math.round((double) sum / count) / 10.0, max / 10.0);
+        }
+
+        @Override
+        public boolean equals(Object that) {
+            return that.getClass() == StationStats.class && ((StationStats) that).name.equals(this.name);
+        }
+
+        @Override
+        public int compareTo(StationStats that) {
+            return name.compareTo(that.name);
+        }
+    }
+}
