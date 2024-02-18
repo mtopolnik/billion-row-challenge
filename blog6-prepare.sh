961dffd02e4c627a31e9876059ec377b8cc89d0e
Marko Topolnik <marko.topolnik@gmail.com>
2024-02-18 19:49:19 +0100

    Fix prepare script

--- blog6-prepare.sh
+++ blog6-prepare.sh
@@ -5,5 +5,5 @@ sdk use java 21.0.2-graal 1>&2
 
 if [ ! -f out/Blog6_image ]; then
     NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -H:+UnlockExperimentalVMOptions -H:-GenLoopSafepoints -march=native --enable-preview -H:InlineAllBonus=10 -H:-ParseRuntimeOptions --initialize-at-build-time=Blog6"
-    native-image $NATIVE_IMAGE_OPTS -o out/Blog6_image Blog6
+    native-image $NATIVE_IMAGE_OPTS -cp out -o out/Blog6_image Blog6
 fi
